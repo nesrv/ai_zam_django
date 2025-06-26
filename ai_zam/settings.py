a96e48e249d7d11967fd74e24b9184b1c398e2b0
ne_srv <ne_srv@mail.ru>
2025-06-26 14:14:15 +0300

    deploy+

--- ai_zam/settings.py
+++ ai_zam/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-scy!f9+jk#j61#hw16ube_z6!ss#wlnno+&-fh4&(v^%&ipuk&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["https://programism.ru/", "programism.ru", "www.programism.ru", "127.0.0.1"]
 
 
 # Application definition
@@ -116,7 +116,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
--- ai_zam/urls.py
+++ ai_zam/urls.py
@@ -1,21 +1,13 @@
-"""
-URL configuration for ai_zam project.
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+
+from django.conf import settings
+from django.conf.urls.static import static
+
+from django.views.static import serve as mediaserve
+
 from object.views import home, objects_list, object_detail, update_expense
 
 # Изменяем заголовки админки
@@ -30,3 +22,14 @@ urlpatterns = [
     path('objects/<int:object_id>/', object_detail, name='object_detail'),
     path('update-expense/', update_expense, name='update_expense'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+else:
+    urlpatterns += [
+        re_path(f'^{settings.MEDIA_URL.lstrip("/")}(?P<path>.*)$',
+            mediaserve, {'document_root': settings.MEDIA_ROOT}),
+        re_path(f'^{settings.STATIC_URL.lstrip("/")}(?P<path>.*)$',
+            mediaserve, {'document_root': settings.STATIC_ROOT}),
+    ]
